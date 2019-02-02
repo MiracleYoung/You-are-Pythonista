5a6bbefacd6115a62ddf47c5f4535dc3fd10c293
eveyrdayx <everydayx@163.com>
2019-02-02 23:58:19 +0800

    20190202 代码注释更新修正

--- PythonExercise/Operation/nagios_check_tomcat/nagios_check_tomcat.py
+++ PythonExercise/Operation/nagios_check_tomcat/nagios_check_tomcat.py
@@ -5,20 +5,31 @@ import urllib2
 import sys
 import os
 
-url = 'http://127.0.0.1/index.html'  # 这里填入你需要检测的web页面
-req = urllib2.Request(url)  # 调用urllib2方法
+# 这里填入你需要检测的web页面
+url = 'http://127.0.0.1/index.html'
+# 调用urllib2方法
+req = urllib2.Request(url)
 try:
-    fp = urllib2.urlopen(req)  # 调用urllib2方法
+    # 调用urllib2方法
+    fp = urllib2.urlopen(req)
     reault = fp.read()
-    if not reault:  # 如果get不到结果就如下处理
+    # 如果get不到结果就如下处理
+    if not reault:
         print "\033[31mtomcat was stopped!\033[0m"
-        os.system('service tomcat restart')  # 调用linux shell命令
-        sys.exit(2)  # 退出，返回2状态码
-except urllib2.URLError, urllib2.HTTPError:  # 如果捕获到http异常，就如下处理
+        # 调用linux shell命令
+        os.system('service tomcat restart')
+        # 退出，返回2状态码
+        sys.exit(2)
+# 如果捕获到http异常，就如下处理
+except urllib2.URLError, urllib2.HTTPError:
     print "\033[31mtomcat was stopped!\033[0m"
-    os.system('service tomcat restart')  # linux shell命令
-    sys.exit(2)  # 退出，返回2状态码
+    # linux shell命令
+    os.system('service tomcat restart')
+    # 退出，返回2状态码
+    sys.exit(2)
 
-print "tomcat is work"  # 如果上面的代码块都没执行到，说明tomcat服务正常运行!"
-sys.exit(0)  # 退出，返回0状态码
+# 如果上面的代码块都没执行到，说明tomcat服务正常运行!"
+print "tomcat is work"
+# 退出，返回0状态码
+sys.exit(0)
 
--- PythonExercise/Operation/ssh_execute_concurrent/ssh_execute_concurrent.py
+++ PythonExercise/Operation/ssh_execute_concurrent/ssh_execute_concurrent.py
@@ -2,24 +2,15 @@
 # coding:utf8
 
 
-#导入各种库文件
-
-#导入日志库
 import logging
-#导入ssh库
 import paramiko
-#导入命令行参数库
 import argparse
-#导入内置sys库
 import sys
-#导入线程池库
 from concurrent import futures
-#导入时间库
 import time
-#导入进度条库
 from progressbar import *
 
-#想知道底下代码具体作用的，参见https://www.jianshu.com/p/9686bfa7e81c
+# 具体作用参见https://www.jianshu.com/p/9686bfa7e81c
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -32,11 +23,11 @@ class run(object):
         '''
         生成对象时的初始化操作
         '''
-        #导入update_file.py这个配置文件的getPlatform函数，详见配置文件模板
+        # 导入update_file.py这个配置文件的getPlatform函数，详见配置文件模板
         from update_file import getPlatform
-        #获取配置文件列表
+        # 获取配置文件列表
         self.host_lists = getPlatform()
-        #定义日志打印格式、输出文件文件、日志级别等
+        # 定义日志打印格式、输出文件文件、日志级别等
         logging.basicConfig(level=logging.INFO,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
@@ -50,12 +41,12 @@ class run(object):
         需要模块用法的请参见：https://blog.ixxoo.me/argparse.html
         '''
         parse = argparse.ArgumentParser()
-        #定义需要执行的线程数参数:-t
+        # 定义需要执行的线程数参数:-t
         parse.add_argument('--thread-number', '-t', dest='num_value', help='thread number')
-        #定义需要执行的命令行参数:-c
+        # 定义需要执行的命令行参数:-c
         parse.add_argument('--command-line', '-c', dest='cmd_value', help='command line')
         args = parse.parse_args()
-        #返回args对象
+        # 返回args对象
         return args
 
 
@@ -63,16 +54,16 @@ class run(object):
         '''
         从update_file.py这个文件的getPlatform函数内获取服务器列表,详见配置文件模板
         '''
-        #定义一个空的列表
+        # 定义一个空的列表
         update_list = []
-        #循环外层列表 ，也就是[['web',(1,100)]，['mysql',(1,10)]]
+        # 循环外层列表 ，也就是[['web',(1,100)]，['mysql',(1,10)]]
         for host_name in self.host_lists:
-            #循环内层列表，也就是web-001,web-002,web-003...；mysql-001,mysql-002,mysql-003...）
+            # 循环内层列表，也就是web-001,web-002,web-003...；mysql-001,mysql-002,mysql-003...）
             for num in range(int(host_name[1][0]), int(host_name[1][1]) + 1):
-                #循环得到的结果，添加至update_list列表
+                # 循环得到的结果，添加至update_list列表
                 update_list.append('%s-%03d' % (host_name[0], num))
 
-        #返回这个列表
+        # 返回这个列表
         return update_list
 
 
@@ -80,33 +71,33 @@ class run(object):
         '''
         将从update_file.py配置文件,getPlatform函数里获取的服务器列表与/etc/hosts文件对比，如果有匹配的就输出到新的列表
         '''
-        list1 = []
+        final_list = []
         hosts_lists = []
-        #调用上面那个函数的返回结果
+        # 调用上面那个函数的返回结果
         file_lists = self.get_hosts()
-        #打开操作系统的hosts文件
+        # 打开操作系统的hosts文件
         with open('/etc/hosts', 'rt') as f:
-            #遍历hosts文件内容
+            # 遍历hosts文件内容
             for line in f.readlines():
-                #这里之所以要增加判断，是为了避免hosts文件里有异常的不合法的结果
+                # 这里之所以要增加判断，是为了避免hosts文件里有异常的不合法的结果
                 if line and  len(line.split()) >= 2:
                     hosts_lists.append(line.split()[1])
         for x in file_lists:
-            #将配置文件里的服务器列表循环，如果不存在于hosts文件，跳过，不然就加入新的列表（也就是file_lists和hosts_lists两个列表取交集，结果存入list1）
+            # 将配置文件里的服务器列表循环，如果不存在于hosts文件，跳过，不然就加入新的列表（也就是file_lists和hosts_lists两个列表取交集，结果存入list1）
             if x not in hosts_lists:
                 continue
-	        list1.append(x)
-        #返回最终配置文件与hosts里取的结果
-        return list1
+            final_list.append(x)
+        # 返回最终配置文件与hosts里取的结果
+        return final_list
 
 
     def remote_ssh(self, ip, cmd):
         '''
-        ssh方法定义，参见https://blog.csdn.net/songfreeman/article/details/50920767
+        ssh方法定义，参见https://qxf2.com/blog/ssh-using-python-paramiko/
         '''
-        #创建SSH对象
+        # 创建SSH对象
         client = paramiko.SSHClient()
-        #把要连接的机器添加到known_hosts文件中
+        # 把要连接的机器添加到known_hosts文件中
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             client.connect(ip)
@@ -115,11 +106,11 @@ class run(object):
             new_std_out = stdout.read()
             new_std_err = stderr.read()
             client.close()
-            #日志里记录执行过的ip和命令行标准、错误输出
+            # 日志里记录执行过的ip和命令行标准、错误输出
             logging.info('{}\n{}'.format(ip,new_std_out))
             logging.error('{}\n{}'.format(ip,new_std_err))
         except:
-            #这里捕获所有的异常
+            # 这里捕获所有会产生的异常，并打印进日志
             logging.error('没连上这个服务器')
         return new_std_out,new_std_err
 
@@ -129,11 +120,12 @@ class run(object):
         '''
         最终实现逻辑的主函数
         '''
-        #获取最终存在的服务器更新列表
+        # 获取最终存在的服务器更新列表
         self.update_hosts = self.get_exists_host()
         try:
-            #linux命令行上执行时输出打印信息（让脚本使用者能看的清楚明了）
+            # linux命令行上执行时输出打印信息（让脚本使用者能看的清楚明了）
             r = raw_input('本次需要更新以下服务器：{},服务器数量：{}，服务器更新命令：{}，确认按1：'.format(self.host_lists,len(self.update_hosts),cmd))
+            # 判断用户输入是否合法，输入不合法退出程序
             if len(r) ==  1 and r.isdigit() and int(r) == 1:
                     pass
             else:
@@ -144,39 +136,69 @@ class run(object):
         obj_dict = dict()
         total_host = len(self.update_hosts)
         current_hostname = 1
-        #进度条相关处理，具体用法可以参见https://blog.csdn.net/dcrmg/article/details/79525167
+        # 进度条相关处理，具体用法可以参见http://www.cnblogs.com/hongfei/p/3982259.html(这个讲的比较好，还有动图)
         progress = ProgressBar()
         proceed_progress = progress(range(total_host))
-        #多线程相关处理，具体用法可以参见https://www.jianshu.com/p/e3a2df19c41a
+
+        # 多线程相关处理，具体用法可以参见
+        # 这里创建了线程池，max_workers是默认的处理线程个数
+        # 参见https://docs.python.org/3/library/concurrent.futures.html
+        '''
+        futures.ThreadPoolExecutor的官方解释如下：
+        class concurrent.futures.ThreadPoolExecutor(max_workers=None, thread_name_prefix='', initializer=None, initargs=())
+        An Executor subclass that uses a pool of at most max_workers threads to execute calls asynchronously.
+        initializer is an optional callable that is called at the start of each worker thread; initargs is a tuple of arguments passed to the initializer. 
+        Should initializer raise an exception, all currently pending jobs will raise a BrokenThreadPool, as well as any attempt to submit more jobs to the pool.
+        '''
         with futures.ThreadPoolExecutor(max_workers=int(thread_num)) as executor:
-            for ip in self.update_hosts:
+            for hostname in self.update_hosts:
+                # 得到异步提交的future对象(也就是executor)的结果
                 e = executor.submit(method,ip,cmd)
-                obj_dict[e] = ip
+                # 生成对象结果与主机名相对应的字典
+                obj_dict[e] = hostname
+
+            # concurrent.futures.as_completed返回了一个迭代器，yield那些完成的futures对象
+            # 任何futures在调用as_completed()之前完成首先被yield。
+            # 参见https://docs.python.org/3/library/concurrent.futures.html
+            '''
+            concurrent.futures.as_completed的官方解释如下：
+            concurrent.futures.as_completed(fs, timeout=None)
+            Returns an iterator over the Future instances (possibly created by different Executor instances) given by fs that yields futures as they complete (finished or were cancelled). 
+            Any futures given by fs that are duplicated will be returned once. 
+            Any futures that completed before as_completed() is called will be yielded first. 
+            The returned iterator raises a concurrent.futures.
+            TimeoutError if __next__() is called and the result isn’t available after timeout seconds from the original call to as_completed(). 
+            timeout can be an int or float. 
+            If timeout is not specified or None, there is no limit to the wait time.
+            '''
             for future in futures.as_completed(obj_dict):
+                # 从futures.as_completed返回的迭代器中获取主机名字
                 host_name = obj_dict[future]
+                # 捕获异常
                 if future.exception() is not None:
                     logging.error('generated an exception: %s' % (future.exception()))
                 else:
-                    n,e = future.result()
-                    #日志里实时显示进度
+                    # 获取结果
+                    n , e = future.result()
+                    # 日志里打印实时进度
                     logging.info('percent: {1:0.2f}%,  excuting host: {0} '.format(host_name,(current_hostname/float(total_host))*100))
-                    #每循环一次，进度条走一格
+                    # 每循环一次，进度条走一格
                     proceed_progress.__next__()
-                    current_hostname +=1
+                    # 用于显示实时脚本执行进度
+                    current_hostname += 1
         try:
-            #进度条结束，捕获迭代异常
+            # 进度条结束，捕获迭代异常
             proceed_progress.__next__()
         except StopIteration:
-            #linux命令行上输出换一行，显得美观
+            # linux命令行上输出换一行，显得美观
             print
 
 
 
 
 if __name__ == '__main__':
-    #生成名称为AAA的对象
-    AAA = run()
-    #生成参数对象（argparse模块）
-    args = AAA.argument()
-    #主函数（分别给主函数传入：命令行获取到的参数，ssh方法，命令行获取线程数（默认为1））
-    AAA.main(args.cmd_value, AAA.remote_ssh, args.num_value)
+    generate_obj = run()
+    # 生成参数对象（argparse模块）
+    args = generate_obj.argument()
+    # 执行主函数部分（分别给主函数传入：命令行获取到的参数，ssh方法，命令行获取线程数参数）
+    generate_obj.main(args.cmd_value, generate_obj.remote_ssh, args.num_value)

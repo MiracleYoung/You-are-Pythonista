b12a0436248c50113cce9281087bbbc5d4d7a514
wubaozhen <48089866+wubaozhen@users.noreply.github.com>
2019-06-07 20:46:25 +0800

    Create matrixtrans.py

---
+++ Homework/wbz/lz_episode_02/matrixtrans.py
@@ -0,0 +1,18 @@
+def matrix(L):
+    '''
+    利用索引值相同取每个子列表上的元素，组合成一个新的子列表
+    :param L: 传入一个矩阵
+    :return: 返回倒置的矩阵
+    '''
+    L_trans = []
+    lenth = len(L[0])  # 得到循环次数
+    for i in range(lenth):
+        temp = []  # 存二级列表
+        for l in L:
+            temp.append(l[i])
+        L_trans.append(temp)
+    return L_trans
+L = [[1,2],[3,4],[5,6]]
+print(matrix(L))
+
+![image]()

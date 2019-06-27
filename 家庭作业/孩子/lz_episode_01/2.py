e51e9a170e10e173fb1af488db1d1101ca3e2baf
punchxu <43167427+punchxu@users.noreply.github.com>
2019-06-27 21:32:09 +0800

    2.py
    求任意整数的几何级数

---
+++ 家庭作业/孩子/lz_episode_01/2.py
@@ -0,0 +1,16 @@
+a = input("请输入第一项a：")
+q = input("请输入要乘的固定比q：")
+n = input("请输入需要要求和的n：")
+
+s = 0
+for i in range(int(n)):
+    s = s + int(a) * pow(int(q),i)
+print(s)
+
+
+
+#result
+请输入第一项a：2
+请输入要乘的固定比q：3
+请输入需要要求和的n：4
+80

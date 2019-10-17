66c85087c58094038edae5172be81678bee41c63
AlainOUYANG <ouyang2013sc@gmail.com>
2019-10-17 11:23:36 +0200

    Modified code

--- Homework/AlainOUYANG/lz_episode_01/prime.py
+++ Homework/AlainOUYANG/lz_episode_01/prime.py
@@ -2,7 +2,7 @@
 @Author: AlainOUYANG
 @Date: 2019-10-16 16:45:49
 @LastEditors: AlainOUYANG
-@LastEditTime: 2019-10-17 11:13:23
+@LastEditTime: 2019-10-17 11:23:23
 @Description: get_prime
 '''
 from math import sqrt
